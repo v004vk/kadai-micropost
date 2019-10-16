f94d88c338906623990e6971883b6788888ee029
soji.inohara <v004vk@gmail.com>
2019-10-16 07:51:42 +0000

    user signup

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,8 @@
+module UsersHelper
+  def gravatar_url(user, option = { size: 80 })
+    #gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_id = Digest::MD5::hexdigest(user.email)
+    size = option[:size]
+    "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+  end
+end
