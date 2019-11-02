ef6456656769dd45e19bd2e4f1b67623930d8bdf
soji.inohara <v004vk@gmail.com>
2019-11-02 05:20:55 +0000

    favorite

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,12 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  has_many :favorites
+  has_many :users_who_favorites_this, through: :favorites, source: :user
   
   validates :content, presence: true, length: { maximum: 255 }
+  
+  def destroy
+    self.favorites.delete_all
+    super
+  end
 end
